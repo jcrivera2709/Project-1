22d7cf146e1747e7c8c4d965d4cee24b2679f639
DemonTailes <DemonTailes@172.26.209.173>
2018-10-12 10:21:56 -0400

    10/12/18

--- Project/src/first.java
+++ Project/src/first.java
@@ -7,48 +7,43 @@ import java.util.Scanner;
 // downloaded java-google-style and Cntrl+Shift+F automatically formatts the project.
 public class first {
 
-
-
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Hello, How can I help?");
-    // int num1;
-    // Person john = new Person();
-    // john.height = 72;
-    // System.out.println();
-
+    /*
+     * int num1; // Person john = new Person(); // john.height = 72; // System.out.println();
+     */
     // Integer saves a number/integer with no fraction number to a stated variable.
-   System.out.println("How large is your party?"); 
-   //final makes the variable immutable. Casting allows me to make a number into another value.
+    System.out.println("How large is your party?");
+    // final makes the variable immutable. Casting allows me to make a number into another value.
     final int party = scan.nextInt();
-    
+
+    /*
+     * Here I am using an if/else statement with conditional operators to create conditions of how
+     * large the party is
+     */
     if (party < 5) {
-    	System.out.println("Come Right this way please.");
+      System.out.println("Come Right this way please.");
+    } else if (party >= 5 && party < 10) {
+      int timewait = party * 2;
+      System.out.println("The wait is going to be " + timewait);
+    } else {
+      int timewait = party * 3;
+      System.out.println("The wait time will be " + timewait);
     }
-    else if(party >= 5 && party < 10) {
-    	int timewait = party * 2;
-    	System.out.println("The wait is going to be " + timewait);
-    }
-    else {
-    	int timewait = party * 3;
-    	System.out.println("The wait time will be " + timewait);
-    }
-   
 
-    
-
-    // String saves a series of characters to a stated variable.
+    // String saves a series of characters to a declared variable.
     String str1 = "So we have ";
     String str2 = "six different pizzas for you today.";
 
     System.out.println(str1 + str2);
-    
-    //Double saves numbers with decimals/fractional numbers to a stated variable.
+
+    // Double saves numbers with decimals/fractional numbers to a stated variable.
     double doub1 = 4.2;
     double doub2 = 7.8;
     double doub3 = doub1 + doub2 + party;
-    
-    //Boolean is an expression that can type a distinguish between a true or false statement.
+
+    // Boolean is an expression that can type a distinguish between a true or false statement.
     if (doub3 > 22) {
       System.out.println("We have a special for parties of 10 and above.");
     }
@@ -65,12 +60,13 @@ public class first {
     System.out.println(str3 + str3.length() + " toppings.");
 
     String str4;
-    str4 = "So if you look at the menu please select a number from 1 through 5 to select your pizza.";
+    str4 =
+        "So if you look at the menu please select a number from 1 through 5 to select your pizza.";
 
     System.out.println(str4.toUpperCase());
 
 
-
+    /* Here I am using a switch statement to get a selection of pizza from a single customer */
     int selection = scan.nextInt();
     String statement;
 
@@ -98,58 +94,97 @@ public class first {
         break;
     }
     System.out.println(statement);
-    
+
     System.out.println("Enter pizza diameter:");
     double pizzaDiameter = scan.nextInt();
     System.out.println("Enter pizza price:");
-    double pizzaPrice =scan.nextInt();
-   
+    double pizzaPrice = scan.nextInt();
+
+    /*
+     * Here I'm calling the method calculatePricePerinch using pizzaDiameter and pizzaPrice as
+     * arguments
+     */
     double pricePerInch = calculatePricePerInch(pizzaDiameter, pizzaPrice);
     System.out.println("Pizza price per inch = " + pricePerInch);
-    
+
     System.out.println("Heres the random Number of the day! ");
     System.out.println(Math.random());
-    
-    System.out.println("And would anyone like something to drink?");
-    int drinks1 = scan.nextInt();
-    if (drinks1%2 == 0) {
-    	System.out.println("Alright coming right up.");
+
+    System.out.println("*Time passes and everyone finishes their meal*");
+
+    System.out.println("Alright and how many checks are we splitting this into?");
+
+    int check = scan.nextInt();
+    String statement2;
+
+    switch (check) {
+      case 1:
+        statement2 = "Aren't you all a happy family.";
+        break;
+      case 2:
+        statement2 = "Half and half";
+        break;
+      case 3:
+        statement2 = "Three is a party in my book ;)";
+        break;
+      case 4:
+        statement2 = "Alright no problem.";
+        break;
+      case 5:
+      case 6:
+        statement2 = "Sir, yes sir";
+        break;
+      default:
+        statement2 = "Alright, I'll be right back.";
+        break;
     }
-    else {
-    	System.out.println("Are you sure I can't get you another drink? It will come out cheaper.");
+
+    System.out.println(statement2);
+
+    /*
+     * Here is another if/else conditional statement with other operators to find the total of the
+     * check
+     */
+    int tip;
+    if (check <= 2) {
+      check = check++;
+      tip = check + 40;
+    } else if (check > 2 && check <= 6) {
+      check += check;
+      tip = check + 25;
+    } else if (check > 6 && check <= 10) {
+      check = check--;
+      tip = check + 15;
+    } else {
+      tip = check + 10; 
     }
-   
-    System.out.println("Alright here is your pizza would you like cheese on that?");
-    String cheese = scan.nextLine();
-    cheese = scan.nextLine();
-    String answer;
-  
-    switch(cheese) {
-    case "yes":
-    	answer = "Okay";
-    	break;
-    case "no":
-    	answer = "Why not?";
-    	break;
-    default:
-    	answer = "What did you say?";
-    }
-    System.out.println(answer);
-    
+    System.out.println("Alright here is the equal total for each check $" + tip);
+
+    String foodString1 = "";
+    String foodString2 = "";
+
+    /*
+     * Evaluates to true because the two previous variables are equal but here I'm using it for a
+     * cheesy quote
+     */
+    System.out.println("Stay " + foodString1.equals(foodString2) + " to youreslf!");
+
+    scan.close();
   }
-    
-    public static double calculatePricePerInch(double diameter, double price) {
-      
-      double ppi;
-      double radius = diameter / 2.0 ;
-      double area = Math.PI * Math.pow(radius, 2);
-      ppi = price / area;
-      return ppi;
+
+
+  // In this method we are accepting diameter and price as parameters and returning a double value
+  public static double calculatePricePerInch(double diameter, double price) {
+
+    double ppi;
+    double radius = diameter / 2.0;
+    double area = Math.PI * Math.pow(radius, 2);
+    ppi = price / area;
+    return ppi;
 
   }
-    
-    
-    
-  
+
+
+
 }
 
