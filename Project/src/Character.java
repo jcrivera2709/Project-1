36b7927ef5d28a3d67476b0970a631917282fed0
jcrivera2709 <jcrivera2709@7127R4S4.STUDENT.ad.fgcu.edu>
2018-10-25 09:43:41 -0400

    Yo

---
+++ Project/src/Character.java
@@ -0,0 +1,19 @@
+
+public class Character {
+  private String name;
+  private int health;
+  
+  public void setName(String person) {
+    name = person;
+  }
+  public void setHealth(int HP) {
+    health = HP;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  public int health() {
+    return health;
+  }
+}
